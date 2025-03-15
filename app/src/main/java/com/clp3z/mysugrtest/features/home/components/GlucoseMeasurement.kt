f269afd55ea334d93ef28a7ff8188a1e5b110811
Clelia <clelia.clp3z@gmail.com>
2025-03-15 14:05:09 +0100

    feat: Added unit radio group and glucose measurement composables

---
+++ app/src/main/java/com/clp3z/mysugrtest/features/home/components/GlucoseMeasurement.kt
@@ -0,0 +1,46 @@
+package com.clp3z.mysugrtest.features.home.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.clp3z.mysugrtest.R
+import com.clp3z.mysugrtest.framework.theme.Spacing
+
+@Composable
+fun GlucoseMeasurement(
+    average: Float,
+    selectedUnit: String
+) {
+    Surface (
+        shape = RoundedCornerShape(Spacing.spacing_4),
+        color = Color.LightGray.copy(alpha = 0.3f),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        Text(
+            text = stringResource(R.string.your_average_is, average, selectedUnit),
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyMedium.copy(color = Color.DarkGray),
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(Spacing.spacing_16)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun GlucoseMeasurementPreview() {
+    GlucoseMeasurement(
+        average = 100f,
+        selectedUnit = "mg/dl"
+    )
+}
---
+++ app/src/main/java/com/clp3z/mysugrtest/features/home/components/UnitRadioGroup.kt
@@ -0,0 +1,63 @@
+package com.clp3z.mysugrtest.features.home.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import com.clp3z.mysugrtest.R
+import com.clp3z.mysugrtest.framework.theme.BoxPreview
+import com.clp3z.mysugrtest.framework.theme.Spacing
+
+@Composable
+fun UnitRadioGroup(
+    onUnitSelected: (String) -> Unit
+) {
+    val units = listOf(stringResource(R.string.mg_dl), stringResource(R.string.mmol_l))
+    var selected by remember { mutableIntStateOf(0) }
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        units.forEachIndexed { index, unit ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(end = Spacing.spacing_16)
+            ) {
+                RadioButton(
+                    selected = selected == index,
+                    onClick = {
+                        selected = index
+                        onUnitSelected(unit)
+                    }
+                )
+                Text(
+                    text = unit,
+                    style = MaterialTheme.typography.bodyLarge,
+                    fontWeight = FontWeight.Normal
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun UnitRadioGroupPreview() {
+    BoxPreview {
+        UnitRadioGroup(onUnitSelected = {})
+    }
+}
