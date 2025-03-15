681fcacc7299bd45d903f1dafa6e9563d84e427e
Clelia <clelia.clp3z@gmail.com>
2025-03-15 20:11:44 +0100

    fix: Fixed hilt dependency injection issues

--- app/src/main/java/com/clp3z/mysugrtest/MainActivity.kt
+++ app/src/main/java/com/clp3z/mysugrtest/MainActivity.kt
@@ -4,15 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.clp3z.mysugrtest.framework.theme.MysugrtestTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.clp3z.mysugrtest.features.measurements.HomeLayout
+import com.clp3z.mysugrtest.framework.theme.AppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MysugrtestTheme {
-
+            AppTheme {
+                Scaffold {
+                    Box(modifier = Modifier.padding(it)) {
+                        HomeLayout()
+                    }
+                }
             }
         }
     }
---
+++ app/src/main/java/com/clp3z/mysugrtest/MySugrApp.kt
@@ -0,0 +1,7 @@
+package com.clp3z.mysugrtest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MySugrApp : Application()
