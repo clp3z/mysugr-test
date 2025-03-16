ab375ea5330c611b765366f5395cfc7dd2f99c17
Clelia <clelia.clp3z@gmail.com>
2025-03-16 15:39:31 +0100

    refactor: Removed magic number from Conversion

--- app/src/main/java/com/clp3z/mysugrtest/features/common/Conversion.kt
+++ app/src/main/java/com/clp3z/mysugrtest/features/common/Conversion.kt
@@ -2,9 +2,11 @@ package com.clp3z.mysugrtest.features.common
 
 import com.clp3z.mysugrtest.entity.GlucoseUnit
 
-fun Float.toMgDl(): Float = this * 18.0182f
+private const val CONVERSION_FACTOR = 18.0182f
 
-fun Float.toMmolL(): Float = this / 18.0182f
+fun Float.toMgDl(): Float = this * CONVERSION_FACTOR
+
+fun Float.toMmolL(): Float = this / CONVERSION_FACTOR
 
 fun  Float.toUnitValue(unit: GlucoseUnit): Float = when (unit) {
     GlucoseUnit.MG_DL -> this.toMgDl()
